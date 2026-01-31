bb277b65a0ef09d631b57209862a5bea9c9a1b16
MikhailRozhenko <mihailkfmn@gmail.com>
2026-01-31 12:14:00 +0000

    Deploying to gh-pages from @ MikhailRozhenko/create-blended-products@93a628abfd0b4ed68f56bf3869891ce323dd37e8 🚀

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-import"./assets/styles-JE8YjOlG.js";import{a as r,i as o}from"./assets/vendor-BKxigkTP.js";const e={categoriesList:document.querySelector(".categories"),productsList:document.querySelector(".products"),divNotFound:document.querySelector(".not-found")};function l(){const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")}function u(){e.categoriesList.addEventListener("click",t=>{if(t.target.nodeName!=="BUTTON")return;const s=t.target.textContent;s==="All"?i():T(s),document.querySelectorAll(".categories__btn").forEach(d=>{d.classList.remove("categories__btn--active")}),t.target.classList.add("categories__btn--active")})}const p="https://dummyjson.com",c=1,n={CATEGORIES:"/products/category-list",PRODUCTS:"/products"};r.defaults.baseURL=p;async function g(){const{data:t}=await r(`${n.CATEGORIES}`);return t}async function _(){const t={limit:12,skip:(c-1)*12},{data:s}=await r.get(`${n.PRODUCTS}`,{params:t});return s}async function m(t){const s={limit:12,skip:(c-1)*12},{data:a}=await r.get(`/products/category/${t}`,{params:s});return a}async function f(t){const s={limit:12,skip:(c-1)*12},{data:a}=await r.get("/products/1",{params:s});return a}function y(t){const s=t.map(a=>`<li class="categories__item">
-   <button class="categories__btn" type="button">${a}</button>
+import"./assets/styles-BKtNv1d4.js";import{a as r,i as n}from"./assets/vendor-BKxigkTP.js";const e={categoriesList:document.querySelector(".categories"),productsList:document.querySelector(".products"),divNotFound:document.querySelector(".not-found"),divModal:document.querySelector(".modal"),divModalProduct:document.querySelector(".modal-product"),btnCloseDivModal:document.querySelector(".modal__close-btn")};function p(){const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")}function m(){e.categoriesList.addEventListener("click",t=>{if(t.target.nodeName!=="BUTTON")return;const s=t.target.textContent;s==="All"?l():T(s),document.querySelectorAll(".categories__btn").forEach(c=>{c.classList.remove("categories__btn--active")}),t.target.classList.add("categories__btn--active")})}const _="https://dummyjson.com",i=1,d={CATEGORIES:"/products/category-list",PRODUCTS:"/products"};r.defaults.baseURL=_;async function g(){const{data:t}=await r(`${d.CATEGORIES}`);return t}async function y(){const t={limit:12,skip:(i-1)*12},{data:s}=await r.get(`${d.PRODUCTS}`,{params:t});return s}async function v(t){const s={limit:12,skip:(i-1)*12},{data:o}=await r.get(`/products/category/${t}`,{params:s});return o}async function f(t){const{data:s}=await r.get(`/products/${t}`);return s}function L(t){const s=t.map(o=>`<li class="categories__item">
+   <button class="categories__btn" type="button">${o}</button>
  </li>
-`).join("");e.categoriesList.innerHTML=s}function b(t){const s=t.map(a=>`<li class="products__item" data-id="${a.id}">
-    <img class="products__image" src="${a.images[0]}" alt="${a.description}"/>
-    <p class="products__title">${a.title}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand: ${a.brand}</span></p>
-    <p class="products__category">Category: ${a.category}</p>
-    <p class="products__price">Price: ${a.price}$</p>
+`).join("");e.categoriesList.innerHTML=s}function b(t){const s=t.map(o=>`<li class="products__item" data-id="${o.id}">
+    <img class="products__image" src="${o.images[0]}" alt="${o.description}"/>
+    <p class="products__title">${o.title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand: ${o.brand}</span></p>
+    <p class="products__category">Category: ${o.category}</p>
+    <p class="products__price">Price: ${o.price}$</p>
  </li>
-`).join("");e.productsList.innerHTML=s}function L(t){const s=t.map(a=>`<li class="products__item" data-id="">
-    <img class="products__image" src="${a.images[0]}" alt="${a.description}"/>
-    <p class="products__title">${a.title}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand: ${a.brand}</span></p>
-    <p class="products__category">Category: ${a.category}</p>
-    <p class="products__price">Price: ${a.price}$</p>
+`).join("");e.productsList.innerHTML=s}function $(t){const s=t.map(o=>`<li class="products__item" data-id="${o.id}">
+    <img class="products__image" src="${o.images[0]}" alt="${o.description}"/>
+    <p class="products__title">${o.title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand: ${o.brand}</span></p>
+    <p class="products__category">Category: ${o.category}</p>
+    <p class="products__price">Price: ${o.price}$</p>
  </li>
-`).join("");e.productsList.innerHTML=s}async function $(){try{const t=await g();y(["All",...t]),l()}catch(t){console.log(t),o.error({message:"Try again later!"})}}async function i(t){try{const s=await _();e.divNotFound.classList.remove("not-found--visible"),b(s.products)}catch(s){console.log(s),o.error({message:"Try again later!"})}}async function T(t){try{const s=await m(t);if(s.products.length===0){e.divNotFound.classList.add("not-found--visible"),e.productsList.innerHTML="";return}e.divNotFound.classList.remove("not-found--visible"),L(s.products)}catch(s){console.log(s),o.error({message:"Try again later!"})}}$();i();u();f().then(t=>console.log(t));
+`).join("");e.productsList.innerHTML=s}function E(t){var c;const s=Array.isArray(t.tags)?t.tags.map(u=>`<li class="modal-product__tag">${u}</li>`).join(""):"",o=`
+    <img class="modal-product__img" src="${((c=t.images)==null?void 0:c[0])||""}" alt="${t.description}" />
+    <div class="modal-product__content">
+      <p class="modal-product__title">${t.title}</p>
+      <ul class="modal-product__tags">${s}</ul>
+      <p class="modal-product__description">${t.description}</p>
+      <p class="modal-product__shipping-information">Shipping: In stock</p>
+      <p class="modal-product__return-policy">Return Policy: 30 days</p>
+      <p class="modal-product__price">Price: ${t.price} $</p>
+      <button class="modal-product__buy-btn" type="button">Buy</button>
+    </div>
+  `;e.divModalProduct.innerHTML=o}async function M(){try{const t=await g();L(["All",...t]),p()}catch(t){console.log(t),n.error({message:"Try again later!"})}}async function l(t){try{const s=await y();e.divNotFound.classList.remove("not-found--visible"),b(s.products)}catch(s){console.log(s),n.error({message:"Try again later!"})}}async function T(t){try{const s=await v(t);if(s.products.length===0){e.divNotFound.classList.add("not-found--visible"),e.productsList.innerHTML="";return}e.divNotFound.classList.remove("not-found--visible"),$(s.products)}catch(s){console.log(s),n.error({message:"Try again later!"})}}async function k(t){try{const s=await f(t);E(s)}catch(s){console.log(s),n.error({message:"Try again later!"})}}function C(){e.productsList.addEventListener("click",t=>{const s=t.target.closest(".products__item");if(!s)return;const o=s.dataset.id;k(o),e.divModal.classList.add("modal--is-open"),e.btnCloseDivModal.addEventListener("click",a),e.divModal.addEventListener("click",a),document.addEventListener("keydown",c=>{c.code==="Escape"&&a()})})}function a(){e.divModal.classList.remove("modal--is-open"),e.btnCloseDivModal.removeEventListener("click",a),document.removeEventListener("keydown",a),e.divModal.removeEventListener("click",a)}M();l();m();C();a();
 //# sourceMappingURL=index.js.map
